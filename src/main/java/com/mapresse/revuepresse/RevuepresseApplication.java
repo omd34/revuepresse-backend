db4938dce949a80432ef29f6d2d2ef9976d9e2ed
omd34 <danielotomo34@gmail.com>
2024-11-06 05:22:34 +0100

    first commit

---
+++ src/main/java/com/mapresse/revuepresse/RevuepresseApplication.java
@@ -0,0 +1,13 @@
+package com.mapresse.revuepresse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevuepresseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevuepresseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapresse/revuepresse/RevuepresseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapresse.revuepresse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevuepresseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
